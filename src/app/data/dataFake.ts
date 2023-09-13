4ebdf3c1c3c46823574388e797d5dec71e8df38a
VitorAndes <vitor.andes.santos04@gmail.com>
2023-09-13 19:09:22 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo Shoujo",
+        "description": "Bom ou ruim?", 
+        "photoCover":"https://nerdhits.com.br/wp-content/uploads/2023/04/WOTAKOI.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Será que ele vai superar ela?",
+        "description": "veremos no proximo ep de Haruaika", 
+        "photoCover":"https://assets.ayobandung.com/crop/0x0:0x0/750x500/webp/photo/2022/05/29/520153340.jpeg"
+    }
+]
